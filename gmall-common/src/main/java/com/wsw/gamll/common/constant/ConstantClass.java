490a6043136fac5e810a325d613473a6d98af2f8
WangZong888 <1846064289@qq.com>
2019-07-20 07:45:52 +0800

    常量类

---
+++ gmall-common/src/main/java/com/wsw/gamll/common/constant/ConstantClass.java
@@ -0,0 +1,11 @@
+package com.wsw.gamll.common.constant;
+
+/**
+ * @program: gmall-parent
+ * @description:
+ * @author: Mr.Wang
+ * @create: 2019-07-20 07:41
+ **/
+public class ConstantClass {
+
+}
